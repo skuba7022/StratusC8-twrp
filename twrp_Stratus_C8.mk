8d3593d5e1d6e1e06b2b874ee7de0efa4d7cc8fe
skuba7022 <skuba7022@gmail.com>
2026-05-30 13:04:19 -0700

    Update twrp_Stratus_C8.mk

--- twrp_Stratus_C8.mk
+++ twrp_Stratus_C8.mk
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
