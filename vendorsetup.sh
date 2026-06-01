e34fb903bef43ba708601c90d48287261b1a6367
skuba7022 <skuba7022@gmail.com>
2026-05-31 19:42:11 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,1 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo omni_Stratus_C8-user
-add_lunch_combo omni_Stratus_C8-userdebug
-add_lunch_combo omni_Stratus_C8-eng
+
